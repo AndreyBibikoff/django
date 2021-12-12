06c54b03da90b2ff2fefb4fb00a8d260b8c5b0b7
Andrey <ab@inteb.pro>
2021-12-13 00:10:42 +0400

    Lesson_2

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from .views import products
 
 urlpatterns = [
-   path('', products),
+   path('', products, name='products'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,4 +3,22 @@ from django.shortcuts import render
 
 # Create your views here./
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'geekshop - каталог'
+    links_menu = [
+        {'href': '#', 'name': 'все'},
+        {'href': '#', 'name': 'дом'},
+        {'href': '#', 'name': 'офис'},
+        {'href': '#', 'name': 'модерн'},
+        {'href': '#', 'name': 'классика'},
+    ]
+    related_products = [
+        {'name': 'лампа', 'img_link': '/static/geekshop/img/product-11.jpg', 'href': '#'},
+        {'name': 'стул', 'img_link': '/static/geekshop/img/product-21.jpg', 'href': '#'},
+        {'name': 'торшер', 'img_link': '/static/geekshop/img/product-31.jpg', 'href': '#'},
+    ]
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+        'related_products': related_products,
+    }
+    return render(request, 'mainapp/products.html', context=context)
