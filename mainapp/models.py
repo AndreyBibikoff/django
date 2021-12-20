6656cd138ef3226e9b38387ceccd0acc812d41dc
Andrey <ab@inteb.pro>
2021-12-20 19:40:32 +0400

    Lesson_3

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ProductCategory(models.Model):
+    category_name = models.CharField(verbose_name='Наименование категории', max_length=64, unique=True)
+    description = models.TextField(verbose_name='Описение', blank=True)
+
+    def __str__(self):
+        return f'{self.category_name} | {self.description}'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Наименование продукта', max_length=64)
+    image = models.ImageField(upload_to='products_image', blank=True)
+    short_descript = models.CharField(verbose_name='Краткое описание', max_length=128, blank=True)
+    description = models.TextField(verbose_name='Описение продукта', blank=True)
+    price = models.DecimalField(verbose_name='Стоимость', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='Количество', default=0)
+
+    def __str__(self):
+        return f'{self.name} - ({self.category.category_name})'
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,23 +2,16 @@ from django.shortcuts import render
 
 
 # Create your views here./
+from mainapp.models import ProductCategory
+
+
 def products(request):
     title = 'geekshop - каталог'
-    links_menu = [
-        {'href': '#', 'name': 'все'},
-        {'href': '#', 'name': 'дом'},
-        {'href': '#', 'name': 'офис'},
-        {'href': '#', 'name': 'модерн'},
-        {'href': '#', 'name': 'классика'},
-    ]
-    related_products = [
-        {'name': 'лампа', 'img_link': '/static/geekshop/img/product-11.jpg', 'href': '#'},
-        {'name': 'стул', 'img_link': '/static/geekshop/img/product-21.jpg', 'href': '#'},
-        {'name': 'торшер', 'img_link': '/static/geekshop/img/product-31.jpg', 'href': '#'},
-    ]
+    products_menu = ProductCategory.objects.all()
+
     context = {
         'title': title,
-        'links_menu': links_menu,
-        'related_products': related_products,
+        'products_menu': products_menu,
+
     }
     return render(request, 'mainapp/products.html', context=context)
